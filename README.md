213a6bcf6f99006e7396d501ca78868360e1aa1a
Tofandel <adrien.foulon@tukan.hu>
2024-10-19 04:07:26 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Installation
+
+First install the composer plugin in your project (or globally)
+
+```bash
+composer require tofandel/wpml-updater
+```
+
+Then create an `auth.json` file in which you will add your WPML credentials 
+```json
+{
+  "get-parameter": {
+    "wpml.org": {
+      "user_id": "",
+      "subscription_key": ""
+    }
+  }
+}
+```
+You can retrieve those credentials by going to https://wpml.org/account/downloads/ 
+and copying the url of any of the plugin's download button and then picking the correct query parameters
+
+Finally you can now install/upgrade any WPML plugin by running
+
+```composer require wpml/sitepress-multilingual-cms```
+
+They will also get upgraded during `composer upgrade` if you do not change the version constraint
+
+> [!IMPORTANT]
+> We do not support rolling back to arbitrary versions, only the latest version is available by default because of the way we retrieve the repository information, so if you want to rollback to a specific version you will need to do so editing your composer.lock manually with the desired version and then running `composer install`
+
+
+### List of available packages
+
+- wpml/sitepress-multilingual-cms
+- wpml/wpml-string-translation
+- wpml/wpml-translation-management
+- wpml/gravityforms-multilingual
+- wpml/contact-form-7-multilingual
+- wpml/wpml-ninja-forms
+- wpml/wpml-wpforms
+- wpml/acfml
+- wpml/wpml-all-import
+- wpml/wpml-mailchimp-for-wp
+- wpml/wpml-media-translation
+- wpml/wp-seo-multilingual
+- wpml/wpml-graphql
+- wpml/wpml-elasticpress
+- wpml/wpml-import
+- wpml/wpml-sticky-links
+- wpml/wpml-cms-nav
+
+We do not provide the plugins available for free on wordpress.org because they are better served by wpackagist.org
---
+++ WPML_Updater.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace WPML_Updater;
+
+use Composer\Composer;
+use Composer\EventDispatcher\Event;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginEvents;
+use Composer\Plugin\PluginInterface;
+use Composer\Plugin\PreCommandRunEvent;
+use Composer\Plugin\PreFileDownloadEvent;
+use Composer\Repository\PackageRepository;
+use Composer\Script\ScriptEvents;
+use Throwable;
+
+class WPML_Updater implements PluginInterface, EventSubscriberInterface
+{
+    protected Composer $composer;
+
+    protected IOInterface $io;
+
+    public function activate(Composer $composer, IOInterface $io): void
+    {
+        $this->composer = $composer;
+        $this->io = $io;
+
+        $existingConfig = $this->composer->getConfig()->get('get-parameter')['wpml.org'] ?? [];
+        if (empty($existingConfig)) {
+            $this->composer->getConfig()
+                ->getLocalAuthConfigSource()?->addConfigSetting('get-parameter.wpml.org', [
+                    "user_id" => "",
+                    "subscription_key" => "",
+                ]);
+        }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            PluginEvents::PRE_COMMAND_RUN => ['registerRepositories', 0],
+            ScriptEvents::PRE_UPDATE_CMD => ['registerRepositories', 0],
+            ScriptEvents::PRE_INSTALL_CMD => ['registerRepositories', 0],
+            PluginEvents::PRE_FILE_DOWNLOAD => ['handlePreDownloadEvent', -1]
+        ];
+    }
+
+    public function registerRepositories(PreCommandRunEvent|\Composer\Script\Event $event): void
+    {
+        static $packages;
+        if ($event instanceof PreCommandRunEvent && !in_array($event->getCommand(), ['update', 'install', 'require'])) {
+            return;
+        }
+        try {
+            if (!isset($packages)) {
+                $remoteJson = json_decode(file_get_contents('https://d2salfytceyqoe.cloudfront.net/wpml33-products.json'));
+
+                $packages = [];
+                foreach ($remoteJson->downloads->plugins as $package) {
+                    if ($package->{'free-on-wporg'} || $package->{'fallback-free-on-wporg'}) {
+                        continue;
+                    }
+                    $packages[] = [
+                        'package' => [
+                            'name' => 'wpml/' . $package->slug,
+                            'type' => 'wordpress-plugin',
+                            'description' => $package->description,
+                            'version' => $package->version,
+                            'require' => [
+                                //'roots/wordpress' => '^' . $package->tested
+                            ],
+                            "dist" => [
+                                "type" => "zip",
+                                "url" => $package->url,
+                            ],
+                        ]
+                    ];
+                }
+                print_r(array_map(fn ($p) => $p['package']['name'], $packages));
+            }
+
+            foreach ($packages as $packageConfig) {
+                ($event instanceof \Composer\Script\Event ? $event->getComposer() : $this->composer)
+                    ->getRepositoryManager()
+                    ->addRepository(new PackageRepository($packageConfig));
+            }
+        } catch (Throwable) {
+            $this->io->error('Could not load WPML plugin information.');
+        }
+    }
+
+    /**
+     * Fulfill package URL placeholders before downloading the package.
+     */
+    public function handlePreDownloadEvent(PreFileDownloadEvent $event): void
+    {
+        static $warned = false;
+
+        $url = $event->getProcessedUrl();
+        $parsed = parse_url($url);
+        $host = $parsed['host'] ?? null;
+        $query = $parsed['query'] ?? null;
+
+        $auth = $this->composer->getConfig()->get('get-parameter');
+        if (!$warned && empty($auth['wpml.org']) && $host === 'wpml.org' && !str_contains($query, 'subscription_key')) {
+            $this->io->warning('Missing authentication parameters for wpml.org, you should set in your auth.json
+{
+  "get-parameter": {
+    "wpml.org": {
+      "user_id": "YOUR_USER_ID",
+      "subscription_key": "YOUR_SUBSCRIPTION_KEY"
+    }
+  }
+}
+');
+        } else if (!$warned) {
+            foreach ($auth as $authHost => $parameters) {
+                if ($host === $authHost) {
+                    $event->setProcessedUrl(
+                        $url . ($query ? '&' : '?') . http_build_query($parameters)
+                    );
+                }
+            }
+        }
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io): void
+    {
+    }
+
+
+    public function uninstall(Composer $composer, IOInterface $io): void
+    {
+    }
+}
